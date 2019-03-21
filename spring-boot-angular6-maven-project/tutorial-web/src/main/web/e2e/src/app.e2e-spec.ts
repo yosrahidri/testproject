83ee3fed73ac0307d5e7f1c92b0f283880761178
Yosra <Yosra@10.2.153.68>
2019-03-21 10:18:52 +0100

    protractor

--- spring-boot-angular6-maven-project/tutorial-web/src/main/web/e2e/src/app.e2e-spec.ts
+++ spring-boot-angular6-maven-project/tutorial-web/src/main/web/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,6 @@
 import { AppPage } from './app.po';
+import {by, browser} from 'protractor';
+
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -11,28 +13,16 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Calculator');
   });
-  
+
   describe('To verify super calculator functionality', () => {
     it('perform addition with 2 values', async () => {
         await browser.get('http://localhost:4200/');
         await browser.element(by.id('firstNumber')).sendKeys('25');
         await browser.element(by.id('secondNumber')).sendKeys('25');
         await browser.element(by.id('sum')).click();
-        expect(await browser.element(by.css('h4')).getText()).toEqual('50')
-    })
+        expect(await browser.element(by.css('h4')).getText()).toEqual('50');
+    });
 });
-  
-  browser.executeAsyncScript(function(callback) {
-  // Here we use document.body, but your app may live under a different
-  // element.
-  var service = angular.element(document.body)
-      .injector()
-      .get('CalculatorService');
-  service.query({}, function(data) {
-    callback(data);
-  });
-}).then(function (output) {
-  console.log(output);
+
 });
-  
-});
+
--- spring-boot-angular6-maven-project/tutorial-web/src/main/web/src/app/calculator/calculator.service.ts
+++ spring-boot-angular6-maven-project/tutorial-web/src/main/web/src/app/calculator/calculator.service.ts
@@ -6,13 +6,13 @@ import { Injectable, Input } from '@angular/core';
 })
 export class CalculatorService {
 
-@Input() first: number;
-@Input() second: number;
+@Input() f: number;
+@Input() s: number;
  constructor() { }
 
-  public calculate(first, second) {
+  public calculate(f, s) {
    let result = 0;
-   result = +first + +second;
+   result = +f + +s ;
    return result;
   }
 
